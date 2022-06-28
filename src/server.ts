6bf74cc36e526704ef038a1717e99b727cecd8e2
Dominic Griesel <dominic.griesel@nabucasa.com>
2022-06-28 13:10:14 +0200

    chore: boilerplate

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import fastifyLib from "fastify";
+
+const fastify = fastifyLib({ logger: true });
+
+// Declare a route
+fastify.get("/", async (request, reply) => {
+	return { hello: "world!" };
+});
+
+// Run the server!
+async function start() {
+	try {
+		await fastify.listen({ port: 3000 });
+	} catch (err) {
+		fastify.log.error(err);
+		process.exit(1);
+	}
+}
+void start();
