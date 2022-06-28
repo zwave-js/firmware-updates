0753a0eafbb2049531033bed626691695790265f
Dominic Griesel <dominic.griesel@nabucasa.com>
2022-06-28 13:53:03 +0200

    fix: bind to correct port

--- src/server.ts
+++ src/server.ts
@@ -9,8 +9,11 @@ fastify.get("/", async (_request, _reply) => {
 
 // Run the server!
 async function start() {
+	let port = parseInt(process.env.PORT!);
+	if (isNaN(port)) port = 3000;
+
 	try {
-		await fastify.listen({ port: 3000 });
+		await fastify.listen({ port });
 	} catch (err) {
 		fastify.log.error(err);
 		process.exit(1);
