259fd11028f954827fc40c70b658bfbe539622e8
AlCalzone <dominic.griesel@nabucasa.com>
2022-09-02 14:48:24 +0200

    fix: persist rate limits across restarts (#12)

--- src/durable_objects/itty.d.ts
+++ src/durable_objects/itty.d.ts
@@ -1,6 +1,6 @@
 type PromisifyPublicFunctions<T> = {
 	[K in keyof T]: T[K] extends (...args: any[]) => any
-		? (...args: Parameters<T[K]>) => Promise<ReturnType<T[K]>>
+		? (...args: Parameters<T[K]>) => Promise<Awaited<ReturnType<T[K]>>>
 		: never;
 };
 
