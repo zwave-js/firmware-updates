cf1c28da689a30c408229c07d9f657ce50327e37
Dominic Griesel <dominic.griesel@nabucasa.com>
2022-09-02 17:20:52 +0200

    fix: apply API keys even when not required

--- src/middleware/withAPIKey.ts
+++ src/middleware/withAPIKey.ts
@@ -1,32 +1,37 @@
+import { error } from "itty-router-extras";
 import { APIKey, decryptAPIKey } from "../lib/apiKeys";
-import { clientError, hex2array } from "../lib/shared";
+import { hex2array } from "../lib/shared";
 import type { CloudflareEnvironment } from "../worker";
 
 export async function withAPIKey(
 	req: Request,
 	env: CloudflareEnvironment
 ): Promise<Response | undefined> {
-	if (env.API_REQUIRE_KEY !== "false") {
-		const keyHex = env.API_KEY_ENC_KEY;
-		if (!keyHex || !/^[0-9a-f]{64}$/.test(keyHex)) {
-			throw new Error("Setup not complete");
+	const fail = (message: string, code: number) => {
+		if (env.API_REQUIRE_KEY !== "false") {
+			return error(code, { error: message });
 		}
-		const key = hex2array(keyHex); // Buffer.from(keyHex, "hex");
+	};
 
-		const apiKeyHex = req.headers.get("x-api-key");
-		if (typeof apiKeyHex !== "string" || !apiKeyHex) {
-			return clientError({ error: "API key not provided" }, 401);
-		}
-
-		let apiKey: APIKey;
-		try {
-			apiKey = await decryptAPIKey(key, apiKeyHex);
-		} catch (e: any) {
-			return clientError({ error: e.message }, 401);
-		}
-
-		(req as any).apiKey = apiKey;
+	const keyHex = env.API_KEY_ENC_KEY;
+	if (!keyHex || !/^[0-9a-f]{64}$/.test(keyHex)) {
+		return fail("Setup not complete", 500);
 	}
+	const key = hex2array(keyHex); // Buffer.from(keyHex, "hex");
+
+	const apiKeyHex = req.headers.get("x-api-key");
+	if (typeof apiKeyHex !== "string" || !apiKeyHex) {
+		return fail("API key not provided", 401);
+	}
+
+	let apiKey: APIKey;
+	try {
+		apiKey = await decryptAPIKey(key, apiKeyHex);
+	} catch (e: any) {
+		return fail(e.message, 401);
+	}
+
+	(req as any).apiKey = apiKey;
 }
 
 export type APIKeyProps = { apiKey?: APIKey };
