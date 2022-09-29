cf5cc2c1c4bd57608cbf6b27fef7a9102800494a
AlCalzone <dominic.griesel@nabucasa.com>
2022-09-29 22:27:36 +0200

    feat: reduce write operations for the rate limiter (#28)

--- src/durable_objects/RateLimiter.ts
+++ src/durable_objects/RateLimiter.ts
@@ -5,9 +5,20 @@ interface RateLimit {
 	remaining: number;
 }
 
+const PERSIST_INTERVAL_MS = 10 * 1000;
+
 export class RateLimiter extends createDurable() {
 	private resetDate: number = 0;
 	private remaining: number = 0;
+
+	private lastPersist: number = 0;
+	private persistTimeout: number | undefined;
+
+	// Avoid persisting the values related to throttling the persist calls
+	public getPersistable(): any {
+		const { persistTimeout, lastPersist, ...persistable } = this;
+		return persistable;
+	}
 
 	private getStorage(): DurableObjectStorage {
 		return (this.state as any).storage;
@@ -19,13 +30,15 @@ export class RateLimiter extends createDurable() {
 	public async request(
 		maxPerHour: number
 	): Promise<RateLimit & { limitExceeded: boolean }> {
-		// Evict the durable object if it hasn't been used for 2 hours
-		await this.getStorage().deleteAlarm();
-		await this.getStorage().setAlarm(Date.now() + 2 * 60 * 60 * 1000);
+		const now = Date.now();
 
-		if (this.resetDate < Date.now()) {
+		// Destroy the durable object if it hasn't been used for 2 hours
+		await this.getStorage().deleteAlarm();
+		await this.getStorage().setAlarm(now + 2 * 60 * 60 * 1000);
+
+		if (this.resetDate < now) {
 			// Not initialized yet or the rate limit has expired
-			this.resetDate = Date.now() + 3600000;
+			this.resetDate = now + 3600000;
 			this.remaining = maxPerHour;
 		}
 
@@ -36,7 +49,7 @@ export class RateLimiter extends createDurable() {
 			limitExceeded = true;
 		}
 
-		await this.persist();
+		await this.throttlePersist();
 
 		return {
 			resetDate: this.resetDate,
@@ -45,11 +58,39 @@ export class RateLimiter extends createDurable() {
 		};
 	}
 
+	private async throttlePersist(): Promise<void> {
+		// Persisting the state on every request is expensive. Instead we persist regularly after changes.
+		// This has the risk of data loss if the worker crashes, or the DO gets evicted early, but that's acceptable for this use case.
+
+		// (Re-)schedule a persist for later, so we automatically persist if nothing happens for a while
+		if (this.persistTimeout) clearTimeout(this.persistTimeout);
+		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+		// @ts-ignore - For some reason, the Node.JS globals end up in this file
+		this.persistTimeout = setTimeout(
+			() => this.doPersist(),
+			PERSIST_INTERVAL_MS
+		);
+
+		// Also make sure to persist busy objects at least every PERSIST_INTERVAL_MS
+		if (
+			this.lastPersist > 0 &&
+			Date.now() - this.lastPersist > PERSIST_INTERVAL_MS
+		) {
+			// We haven't persisted in a while, so persist now
+			await this.doPersist();
+		}
+	}
+
+	private async doPersist(): Promise<void> {
+		await this.persist();
+		this.lastPersist = Date.now();
+	}
+
 	/** Refreshes the rate limiter and sets its remaining requests to the given value */
 	public async setTo(maxPerHour: number): Promise<void> {
 		this.resetDate = Date.now() + 3600000;
 		this.remaining = maxPerHour;
-		await this.persist();
+		await this.doPersist();
 	}
 
 	public async alarm(): Promise<void> {
