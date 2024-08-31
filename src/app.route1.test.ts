e66e28e30d1e3c47110bb5a6e7897fa522a24a41
AlCalzone <dominic.griesel@nabucasa.com>
2024-08-31 14:36:24 +0200

    chore: switch to wrangler v3 (#137)

--- src/app.route1.test.ts
+++ src/app.route1.test.ts
@@ -1,28 +1,25 @@
 import test from "ava";
-import { Miniflare } from "miniflare";
+import { UnstableDevWorker, unstable_dev } from "wrangler";
 
-test.beforeEach((t) => {
-	// Create a new Miniflare environment for each test
-	const mf = new Miniflare({
-		// Autoload configuration from `.env`, `package.json` and `wrangler.toml`
-		envPath: true,
-		packagePath: true,
-		wranglerConfigPath: true,
-		// We don't want to rebuild our worker for each test, we're already doing
-		// it once before we run all tests in package.json, so disable it here.
-		// This will override the option in wrangler.toml.
-		buildCommand: undefined,
+test.before(async (t) => {
+	const worker = await unstable_dev("src/worker.ts", {
+		experimental: { disableExperimentalWarning: true },
 	});
-	t.context = { mf };
+	t.context = { worker };
+});
+
+test.after.always(async (t) => {
+	const { worker } = t.context as any;
+	await worker.stop();
 });
 
 test("GET `/` route", async (t) => {
 	process.env.API_REQUIRE_KEY = "false";
 
-	// Get the Miniflare instance
-	const mf = (t.context as any).mf as Miniflare;
+	// Get the worker instance
+	const worker = (t.context as any).worker as UnstableDevWorker;
 	// Dispatch a fetch event to our worker
-	const res = await mf.dispatchFetch("http://localhost:8787/");
+	const res = await worker.fetch("/");
 
 	t.is(res.status, 200);
 	t.is(res.headers.get("content-type"), "text/html");
